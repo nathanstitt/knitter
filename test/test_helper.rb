8c3b16cec83d00f1c5c512fc37c4cc5f49bc4d72
Nathan Stitt <nathan@stitt.org>
2016-12-08 21:24:40 -0600

    restore fixtures after run

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,21 +1,28 @@
 require "minitest"
 require "minitest/autorun"
 require 'minitest/mock'
-require 'spy' #/integration'
+require 'spy/integration'
+require 'pathname'
 
 require_relative '../lib/knitter'
 
+
 module HelperMethods
+    FIXTURES_PATH =  Pathname.new(__FILE__).dirname.join('fixtures')
     def with_package
-        Dir.mktmpdir do |dir|
-            process = Knitter::Process.new(dir)
-            process.init
-            yield process
-        end
+        yarn = Knitter::Yarn.new(FIXTURES_PATH)
+        pkg  = yarn.package_file.read
+        lock = yarn.directory.join('yarn.lock').read
+        yarn.init unless yarn.valid?
+        yield yarn
+        yarn.package_file.write(pkg)
+        yarn.directory.join('yarn.lock').write lock
     end
 end
 
 class MiniTest::Test
+
+
     include HelperMethods
     alias_method :run_without_around, :run
     def run(*args)
