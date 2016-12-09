ac031c46f18a957927bf6e7175995f09df296461
Nathan Stitt <nathan@stitt.org>
2016-12-08 21:26:12 -0600

    test installing packages

--- test/test_package.rb
+++ test/test_package.rb
@@ -3,29 +3,28 @@ require_relative './test_helper'
 class TestPackage < MiniTest::Test
 
     def around
-        with_package do |process|
-            @process = process
+        with_package do |yarn|
+            @yarn = yarn
             yield
         end
     end
 
     def test_reads_from_package
-        config = JSON.parse(@process.package_file.read)
-        config['dependencies'] = {'foo': "^1.2.3" }
-        @process.package_file.write JSON.dump(config)
-        package = Knitter::Package.new('foo', process: @process)
+        config = JSON.parse(@yarn.package_file.read)
+        config['dependencies'] = {'foo' => "^1.2.3" }
+        @yarn.package_file.write JSON.dump(config)
+        package = Knitter::Package.new('foo', yarn: @yarn)
         assert package.installed?
         assert_equal '^1.2.3', package.version.to_s
         assert_equal :dependencies, package.dependency_type
     end
 
     def test_package_area
-        package = Knitter::Package.new('foo', process: @process)
+        package = Knitter::Package.new('foo', yarn: @yarn)
         package.dependency_type=:optional
         assert_equal :optional, package.dependency_type
         package.dependency_type=:development
         assert_equal :development, package.dependency_type
     end
-
 
 end
--- test/test_yarn.rb
+++ test/test_yarn.rb
@@ -1,6 +1,6 @@
 require_relative './test_helper'
 
-class TestProcess < MiniTest::Test
+class TestYarn < MiniTest::Test
 
     def left_pad
         package = Knitter::Package.new('left-pad')
@@ -10,26 +10,36 @@ class TestProcess < MiniTest::Test
     end
 
     def test_init
-        with_package do |process|
-            assert process.ok?
+        with_package do |yarn|
+            assert yarn.valid?
         end
     end
 
     def test_add_args
-        with_package do |process|
-            spy = Spy.on(process, :execute)
-            process.add(left_pad)
-            assert spy.has_been_called_with?(
-                       "add", "left-pad@~1.1", '--dev'
-                   )
+        with_package do |yarn|
+            spy = Spy.on(yarn, :execute)
+            yarn.add(left_pad)
+            assert spy.has_been_called_with?('add', 'left-pad@~1.1', '--dev')
         end
     end
 
     def test_add_for_reals
-        with_package do |process|
-            process.add(left_pad)
-            assert process.ok?
-            assert_includes(process.stdout, "Saved 1 new dependency")
+        with_package do |yarn|
+            assert_equal 9, yarn.packages.to_a.length
+            yarn.add(left_pad)
+            assert yarn.ok?
+            assert_includes(yarn.stdout, "success")
+            assert_equal 10, yarn.packages.to_a.length
+        end
+    end
+
+    def test_iteration
+        with_package do |yarn|
+            assert_equal 9, yarn.packages.to_a.length
+            qs = yarn.packages.find{|pkg| pkg.name == 'qs' }
+            assert qs
+            assert_equal '6.3.0', qs.version.to_s
+            assert_equal :peer, qs.dependency_type
         end
     end
 
